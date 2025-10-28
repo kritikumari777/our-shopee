757d60fb59e64889fc5db93a082fd5b8dd5cc0df
kriti kumari <123kritikumari@gmail.com>
2025-10-29 00:00:38 +0530

    texts

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,7 +4,7 @@ const ProductCard = ({key, product, onAdd }) => {
     return (
     <div key={key} className="border rounded p-4 shadow-sm flex flex-col">
       <img src={product.image} alt={product.title} className="h-40 object-contain mb-3" />
-      <h3 className="text-sm font-medium mb-2 line-clamp-2">{product.title}</h3>
+      <h5 className="text-sm font-medium mb-2 line-clamp-2">{product.title}</h5>
       <div className="mt-auto">
         <div className="font-semibold">₹ {product.price}</div>
         <button
